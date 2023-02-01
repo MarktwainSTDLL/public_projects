966ab7ff3bcb97c7c1ac47a3d99eb93dafd4dbac
lizaxk <marktwainer2020@gmail.com>
2023-02-01 11:28:57 -0300

    Add files via upload

---
+++ Navbars/Navbar1/assets/js/navbar.js
@@ -0,0 +1,48 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector("header .navbar ul");
+
+hamburger.addEventListener("click", function() {
+    navMenu.classList.toggle('show');
+});
+
+const typedTextSpan = document.querySelector("#specialties");
+const cursorSpan = document.querySelector(".cursor");
+
+const textArray = ["Front-End Developer", "Designer", "C++ Developer", "Skater :)"];
+const typingDelay = 200;
+const erasingDelay = 100;
+const newTextDelay = 2000;
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+    setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+  if (charIndex > 0) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+    textArrayIndex++;
+    if(textArrayIndex>=textArray.length) textArrayIndex=0;
+    setTimeout(type, typingDelay + 1100);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  if(textArray.length) setTimeout(type, newTextDelay + 250);
+});
